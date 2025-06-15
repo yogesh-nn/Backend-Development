bb7181fc0fa1363ffb7d66d5928a3d919f2e9ee6
Yogesh Navandhar <yogesh_navandhar@hotmail.com>
2025-06-15 12:22:57 +0530

    Understanding the process...

--- Practice Basic Express/script.js
+++ Practice Basic Express/script.js
@@ -1,14 +1,29 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 
-app.get("/", function(req, res){
-    res.send("Hello World!");
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
+
+
+
+app.use(function(req, res, next){
+    console.log("Request received at: " + Date.now());
+    next();
 });
 
-app.get("/profile", function(req, res){
-    res.send("This is my profile!!!")
-})
+
+app.get("/", function(req, res){
+    res.render("index");
+});
+
+
+app.get("/profile/:username", function(req, res){
+    res.send(`This is the profile of ${req.params.username}`);
+});
 
 app.listen(3000, function(){
     console.log("Server is running on port 3000");
-});     
+});
